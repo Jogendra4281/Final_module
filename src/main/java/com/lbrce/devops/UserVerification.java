194c2f53e94be5830fa843b1aa36a09e66275be9
Jogendra4281 <152368303+Jogendra4281@users.noreply.github.com>
2024-10-16 23:29:08 +0530

    Add files via upload

---
+++ src/main/java/com/lbrce/devops/UserVerification.java
@@ -0,0 +1,12 @@
+package com.lbrce.devops;
+
+public class UserVerification {
+	public static void check() {
+		System.out.println("hello...!, this is for maven");
+	}
+	public static void main(String args[]) {
+		check();
+	}
+	
+
+}
---
+++ src/test/java/com/lbrce/devopsf/UserVerificationTest.java
@@ -0,0 +1,9 @@
+package com.lbrce.devopsf;
+import com.lbrce.devops.*;
+public class UserVerificationTest extends UserVerification {
+
+	public static void main(String[] args) {
+		check();
+	}
+
+}
